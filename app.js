e19746083812e1fbce42e827c4a723a2245912ed
Iwin <iwint.cseasnsct@gmail.com>
2021-09-09 13:06:12 +0530

    Blog

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
+const blogRouter = require('./routes/blog')
+const contactRouter = require('./routes/contact')
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+app.set('view engine', 'ejs')
+app.use('/views',express.static(__dirname+'/views'))
+app.use('/',express.static(__dirname+'/Public'))
+
+
+
+app.use('/blog',blogRouter)
+app.use('/contact',contactRouter)
+
+app.get('/' , (req , res)=>{
+   
+   res.render('index')
+
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port 3000!`))
