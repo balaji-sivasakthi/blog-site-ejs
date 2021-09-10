0f6d527a089a6af4ea70b66f189de8c717d12e1e
Iwin <iwint.cseasnsct@gmail.com>
2021-09-10 22:08:59 +0530

    Blog And Reviews in Home Page

--- Utils/reviews.js
+++ Utils/reviews.js
@@ -1,26 +1,26 @@
 const db = require('../config').firestore()
 
 //user
-function blog(n){
+function reviews(n){
     return new Promise((resolve,reject)=>{
-        db.collection('blog').get()
+        db.collection('reviews').get()
         .then(result=>{
-            var blog =[]
+            var reviews =[]
             var count =0
             result.forEach(e=>{
                 if(count<n){
                     var data = e.data()
                     data['id'] = e.id
-                    blog.push(data)
+                    reviews.push(data)
                     count++
                 }else{
                     return
                 }
                
             })
-           resolve(blog)
+           resolve(reviews)
         })
     })
 }
 
-module.exports = blog
+module.exports = reviews
--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const GadgetRouter = require('./routes/gadget')
 const ReviewsRouter = require('./routes/reviews')
 const contactRouter = require('./routes/contact')
 const singleRouter = require('./routes/single')
-const port = 3000
+const getblog = require('./Utils/blog')
+const getreviews = require('./Utils/reviews')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -25,11 +26,13 @@ app.use('/video',VideoRouter)
 /app.use('/contact',contactRouter)
 app.use('/single',singleRouter)
 
-app.get('/' , (req , res)=>{
+app.get('/' ,async (req , res)=>{
+   var data = await getblog(15)
+   var reviews = await getreviews(3)
+   res.render('index',{data:data,reviews:reviews})
    
-   res.render('index')
 
 })
 
-
+const port = 3000
 app.listen(port, () => console.log(`Example app listening on port 3000!`))
